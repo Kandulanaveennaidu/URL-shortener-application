17379983723c8c20f796ec4d1e5a42057f7aa4c7
Kandula Naveen <kandulanaveennaidu017@gmail.com>
2024-02-05 21:42:11 +0530

    first.commet

--- client/src/Homepage.jsx
+++ client/src/Homepage.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from "react";
 import { Heading, Text } from "@chakra-ui/react";
 import { InputForm } from "./components/InputForm";
 export default function Homepage() {
   return (
     <>
-          <Heading as="h3" size="xl" m={ '3% 0% 2% 0%' }>
+      <Heading as="h3" size="xl" m={"3% 0% 2% 0%"}>
         URL Shortener
-          </Heading>
-          <InputForm />
-          <Text m={ '3%' }>Made with ❤️ by Me ( <a target="_blank" href="https://akshay-kumar-portfoilo.netlify.app/"> Akshay Kumar</a>)</Text>
+      </Heading>
+      <InputForm />
+      <Text m={"3%"}>
+        Made with ❤️ by Me ({" "}
+        <a target="_blank" href="https://new-portfolio-psi-three.vercel.app/">
+          {" "}
+          Kandula Naveen
+        </a>
+        )
+      </Text>
     </>
-  )
+  );
 }
